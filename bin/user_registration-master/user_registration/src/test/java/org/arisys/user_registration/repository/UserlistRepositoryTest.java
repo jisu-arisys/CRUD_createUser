901107dd6f501b8c154fe293c209029ab544550b
kkssbbb <03337@naver.com>
2023-07-12 09:27:19 +0900

    [user_registration파일 추가] - bin

---
+++ bin/user_registration-master/user_registration/src/test/java/org/arisys/user_registration/repository/UserlistRepositoryTest.java
@@ -0,0 +1,48 @@
+package org.arisys.user_registration.repository;
+
+import org.arisys.user_registration.entity.UsersEntity;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+
+
+
+@SpringBootTest
+public class UserlistRepositoryTest {
+
+    @Autowired
+    private UserlistRepository userlistRepository;
+
+    @Test
+    @DisplayName("유저 디비 인서트 테스트")
+    public void insertTest(){
+        IntStream.rangeClosed(1,200).forEach(i ->{
+            UsersEntity usersEntity = UsersEntity.builder()
+                    .name("김승빈"+ i)
+                    .age(i)
+                    .gender( "남자")
+                    .job("개발자")
+                    .build();
+            System.out.println(userlistRepository.save(usersEntity));
+
+        });
+    }
+
+    @Test
+    @DisplayName("수정시간 적용 테스트")
+    public void updateTest(){
+        Optional<UsersEntity> findUser =  userlistRepository.findById(1L);
+
+        UsersEntity usersEntity =   findUser.get();
+
+        usersEntity.changeGender("여자");
+
+        userlistRepository.save(usersEntity);
+
+    }
+}
